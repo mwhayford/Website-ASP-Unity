5e5de4b9883ecb1ca530e617de307727958c8a5d
mwhayford <mwhayford@gmail.com>
2015-04-03 17:13:16 -0500

    Add initial solution
    Add initial MVC solution and some initial imports of wrap bootstrap
    templates

---
+++ HayfordTechnologies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HayfordTechnologies.Startup))]
+namespace HayfordTechnologies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
